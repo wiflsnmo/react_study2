e6614580fa147c8b56006534c1e92f96e32a5e14
Perry Wu <pan.wu2@thermofisher.com>
2017-07-19 14:02:25 +0800

    config react

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 var webpack = require('webpack')
+var path = require("path")
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: './dist/',
+    path: path.resolve(__dirname, './dist'),
     filename: 'index.js'
-  }
+  },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'stage-0', 'react']
+            }
+        }]
+    }
 }
